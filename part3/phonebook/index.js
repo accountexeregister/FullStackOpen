8e683e06b9b4c5d5b762ee27c04ff169e8ca6acc
accountexeregister <yeohchongyi2@outlook.com>
2023-02-17 16:13:45 +0800

    Complete 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = 
+let persons = 
 [
     { 
       "id": 1,
@@ -45,6 +45,19 @@ app.get('/api/persons/:id', (request, response) => {
     }
 
     response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (!person)
+    {
+        return response.status(404).end()
+    }
+
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
