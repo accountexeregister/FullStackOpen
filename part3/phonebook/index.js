d08427759df668f2f14ec07240100bbda0d3fc5c
accountexeregister <yeohchongyi2@outlook.com>
2023-03-06 01:13:00 +0800

    Complete 3.15 and 3.16

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -14,6 +14,14 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postdata'))
 app.use(express.static('build'))
 
+const errorHandler = (error, request, response, next) => {
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'Not a valid id'})
+    }
+
+    next(error)
+}
+
 app.get('/api/persons', (request, response) => 
     Number.find({}).then(numbers => {
         response.json(numbers)
@@ -29,27 +37,24 @@ app.get('/info', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
     Number.findById(id).then(person => {
         if (person === null) {
             return response.status(404).end()
         }
         response.json(person)
-    })
+    }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (!person)
-    {
-        return response.status(404).end()
-    }
-
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Number.findByIdAndDelete(request.params.id).then(person => {
+        if (person) {
+            response.status(204).end()
+        } else {
+            response.status(404).send({ error: `person with id ${request.params.id} does not exist`})
+        }
+    }).catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -90,5 +95,10 @@ app.put('/api/persons/:id', (request, response) => {
     response.json(updatedPerson)
 })
 
+const unknownRoute = (request, response) => {
+    response.status(404).json({error: "unknown route"})
+}
+app.use(unknownRoute)
+app.use(errorHandler)
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log("Server running on port " + PORT))
