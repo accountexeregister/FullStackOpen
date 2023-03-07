7d6be9767378de3e088f3209061cc8b3d3540e7c
accountexeregister <yeohchongyi2@outlook.com>
2023-03-07 16:45:24 +0800

    Complete 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reduceFunc = (sum, post) => {
+        return sum + post.likes
+    }
+
+    return blogs.reduce(reduceFunc, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
