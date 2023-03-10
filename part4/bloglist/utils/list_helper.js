82cb238977570e7cfbad03eb6339740cd99e93d9
accountexeregister <yeohchongyi2@outlook.com>
2023-03-10 14:56:46 +0800

    Complete 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,7 +10,27 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reduceFunc, 0)
 }
 
+const favouriteBlog = (blogs) => {
+    if (blogs.length == 0) {
+        return null
+    }
+    
+    const reduceFunc = (currentBlog, blog) => {
+        if (blog.likes > currentBlog.likes) {
+            return blog
+        }
+        return currentBlog
+    }
+    const favouriteBlogFull = blogs.reduce(reduceFunc)
+    return { 
+        title: favouriteBlogFull.title, 
+        author: favouriteBlogFull.author, 
+        likes: favouriteBlogFull.likes 
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
